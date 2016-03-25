293bcd3453c250517b122b3b736f4473955b4545
Piotr Bliszczyk <piotr.bliszczyk@gmail.com>
2016-03-25 11:01:47 +0100

    Fix code style; remove jruby from builds

--- lib/easy_passwords/easy_passwords.rb
+++ lib/easy_passwords/easy_passwords.rb
@@ -22,7 +22,7 @@ module EasyPasswords
   DEFAULT_MAX_LENGTH = 12
   MIN_WORD_LENGTH = 3
   MAX_WORD_LENGTH = 6
-  SEPARATORS = "-123456789"
+  SEPARATORS = '-123456789'.freeze
 
   # Public: Returns a random generated password string.
   #
@@ -41,44 +41,42 @@ module EasyPasswords
   # 	# => "spate7Coup"
   #
   # Returns a password string.
-	def self.generate(max_length = DEFAULT_MAX_LENGTH, separators = SEPARATORS)
-		self::Generator.new(separators).generate max_length
-	end
+  def self.generate(max_length = DEFAULT_MAX_LENGTH, separators = SEPARATORS)
+    self::Generator.new(separators).generate max_length
+  end
 
-	def self.new
-		self::Generator.new
-	end
+  def self.new
+    self::Generator.new
+  end
 
-	class Generator
-		def initialize(separators = EasyPasswords::SEPARATORS)
-			@@wordlist_size = @@wordlist.length
-			@@separators = separators
-			@@separators_size = @@separators.length
-			@rand = SecureRandom
-		end
+  class Generator
+    def initialize(separators = EasyPasswords::SEPARATORS)
+      @@wordlist_size = @@wordlist.length
+      @@separators = separators
+      @@separators_size = @@separators.length
+      @rand = SecureRandom
+    end
 
-		# Public: Returns a random generated password string.
-		#
-		# max_length - max number of characters used in password, it could generate password shorter by 3 characters.
-		#
-		# Example
-		#
-		# 	generate 8
-		# 	# => "Fun_Crop"
-		#
-		# 	generate
-		# 	# => "spate7Coup"
-		#
-		# Returns a password string.
-		def generate(max_length = EasyPasswords::DEFAULT_MAX_LENGTH)
-      raise "Password minimal length is #{EasyPasswords::MIN_WORD_LENGTH}" if max_length < EasyPasswords::MIN_WORD_LENGTH
-			output = ""
+    # Public: Returns a random generated password string.
+    #
+    # max_length - max number of characters used in password, it could generate password shorter by 3 characters.
+    #
+    # Example
+    #
+    # 	generate 8
+    # 	# => "Fun_Crop"
+    #
+    # 	generate
+    # 	# => "spate7Coup"
+    #
+    # Returns a password string.
+    def generate(max_length = EasyPasswords::DEFAULT_MAX_LENGTH)
+      fail "Password minimal length is #{EasyPasswords::MIN_WORD_LENGTH}" if max_length < EasyPasswords::MIN_WORD_LENGTH
+      output = ''
       while output.size < (max_length - (EasyPasswords::MIN_WORD_LENGTH - 1))
-        output << random_word(choose_word_length(max_length, output.size))
-
-				output << @@separators[@rand.random_number(@@separators_size)] if output.size < max_length
-			end
-			output
+        add_part(output, max_length)
+      end
+      output
     end
 
     private
@@ -97,12 +95,17 @@ module EasyPasswords
 
     def random_word(maxsize = EasyPasswords::MAX_WORD_LENGTH)
       list = if maxsize < EasyPasswords::MAX_WORD_LENGTH
-        @@wordlist.select{|w| w.size <= maxsize }
-      else
-        @@wordlist
-      end
+               @@wordlist.select { |w| w.size <= maxsize }
+             else
+               @@wordlist
+             end
       word = list[@rand.random_number(list.size)]
-			@rand.random_number(2) == 0 ? word.capitalize : word
+      @rand.random_number(2) == 0 ? word.capitalize : word
     end
-	end
+
+    def add_part(output, max_length)
+      output << random_word(choose_word_length(max_length, output.size))
+      output << @@separators[@rand.random_number(@@separators_size)] if output.size < max_length
+    end
+  end
 end
